aaa83548b9886cc0aeacec3c6a566cb9242a7efa
Katrin889 <kakamitreva@gmail.com>
2025-01-27 14:11:21 +0200

    Add files via upload

---
+++ Moqt klas/Moqt klas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moqt_klas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kolko du[i sa v chas? ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Kolko momcheta shte vyvedesh? ");
+            int b = int.Parse(Console.ReadLine());
+            string[] arr = new string[b];
+            for (int i = 0; i < b; i++)
+            {
+                arr[i] = Console.ReadLine();
+            }
+            if (b >= 10)
+                Console.WriteLine("shte uchim normalno - mycki klas");
+            else if (b < 10)
+                Console.WriteLine("izlizame gripna ot 29");
+            if (a == 29)
+                Console.WriteLine("Vsichki sme");
+            if (a == 14)
+                Console.WriteLine("Grupa sme");
+            else
+                Console.WriteLine("Mnogo sme bolni");
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr[b-1]);
+            Console.WriteLine(arr[(b-1)/2]);
+
+        }
+    }
+}
---
+++ Moqt klas/za 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace za_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kak se kazvash");
+            string[] arr1 = new string[4];
+            for (int i = 0; i < 4; i++)
+            {
+                arr1[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Kolko ocenki imash");
+            int b = int.Parse(Console.ReadLine());
+            int[] arr = new int[b];
+            int c = 0;
+            for (int i = 0; i < b; i++)
+            {
+                Console.Write("ocenka " + (i+1) + " =");
+                arr[i] = int.Parse(Console.ReadLine());
+                c += arr[i];
+            }
+            Console.WriteLine(arr1[1]);
+            Console.WriteLine(arr1[0]);
+            Console.WriteLine(c/b);
+        }
+    }
+}
